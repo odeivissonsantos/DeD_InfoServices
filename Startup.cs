6ce9c3ffa08b27492789b49c3355d5e583400991
deivissonss <deivisson.santos@detran.ba.gov.br>
2022-10-31 14:36:29 -0300

    Inclusão de controller e view para login

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace DeD_InfoServices
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
